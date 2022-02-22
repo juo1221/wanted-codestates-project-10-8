cb3322e81e149e78f1d29b72172288fb31c77f6e
try <dlsduf@gmail.com>
2022-02-22 20:06:43 +0900

    [Feat] reset css 및 prettierrc 적용

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import './App.css';
 import List from './pages/List';
 import Main from './pages/Main';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import GlobalStyles from './GlobalStyles';
 
 function App() {
   return (
     <Router>
+      <GlobalStyles />
       <Routes>
         <Route path="/" element={<Main />} />
         <Route path="/list" element={<List />} />
