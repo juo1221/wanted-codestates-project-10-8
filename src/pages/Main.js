6efc0198e4ff9cb4b936eeb938e9ff8d4133e8b1
try <dlsduf@gmail.com>
2022-02-22 22:36:48 +0900

    [Feat] #4 feedback msg  컴포넌트 구현

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,5 +1,49 @@
-import React from 'react';
+import React, { useState } from 'react';
+import styled from 'styled-components';
+
+import {
+  CompleteRemovedMsg,
+  CompleteSavedMsg,
+  MemoRequestMsg,
+} from '../components/Feedback';
 
 export default function Main() {
-  return <div>Main</div>;
+  const [showFeedbackMemo, setShowFeedbackMemo] = useState(false);
+  const [showFeedbackSave, setShowFeedbackSave] = useState(false);
+  const [showFeedbackRemove, setShowFeedbackRemove] = useState(false);
+
+  const FeedbackHandler = (setter) => {
+    setter(true);
+    setTimeout(() => {
+      setter(false);
+    }, 1000);
+  };
+
+  return (
+    <div>
+      <ButtonStyle onClick={() => FeedbackHandler(setShowFeedbackMemo)}>
+        memo
+      </ButtonStyle>
+      <ButtonStyle onClick={() => FeedbackHandler(setShowFeedbackSave)}>
+        save
+      </ButtonStyle>
+      <ButtonStyle onClick={() => FeedbackHandler(setShowFeedbackRemove)}>
+        remove
+      </ButtonStyle>
+      {showFeedbackMemo && <MemoRequestMsg />}
+      {showFeedbackSave && <CompleteSavedMsg />}
+      {showFeedbackRemove && <CompleteRemovedMsg />}
+    </div>
+  );
 }
+
+const ButtonStyle = styled.button`
+  border: 1px solid black;
+  padding: 20px;
+  :hover {
+    color: red;
+  }
+  :active {
+    opacity: 0.6;
+  }
+`;
