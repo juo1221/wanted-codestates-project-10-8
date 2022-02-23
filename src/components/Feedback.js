0ba73dfae759b25a0b9945c9be1d8d2215441625
try <dlsduf@gmail.com>
2022-02-23 12:47:35 +0900

    [Feat] #22 feedback 메시지 에니메이션기능추가

--- src/components/Feedback.js
+++ src/components/Feedback.js
@@ -2,6 +2,23 @@ import { useEffect, useState } from 'react';
 import styled from 'styled-components';
 
 export function MemoRequestMsg() {
+  const [opacity, setOpacity] = useState(100);
+
+  useEffect(() => {
+    softRemover();
+  }, [softRemover]);
+
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  function softRemover() {
+    if (opacity > 96) {
+      setTimeout(() => {
+        setOpacity(opacity - 1);
+      }, 100);
+    } else if (opacity > 5)
+      setTimeout(() => {
+        setOpacity(opacity - 8);
+      }, 50);
+  }
   return (
     <FeedbackContainerStyle //
       height={'60px'}
@@ -9,6 +26,7 @@ export function MemoRequestMsg() {
       fontSize={'18px'}
       top={'10%'}
       left={'70%'}
+      opacity={`${opacity}%`}
     >
       메모를 입력해주세요.
     </FeedbackContainerStyle>
@@ -16,27 +34,24 @@ export function MemoRequestMsg() {
 }
 
 export function CompleteSavedMsg() {
-  const [opacity, setOpacity] = useState(1);
-  console.log('시작전');
+  const [opacity, setOpacity] = useState(100);
 
-  let animation;
-  // eslint-disable-next-line react-hooks/exhaustive-deps
-  function softRemover() {
-    setOpacity(opacity - 0.05);
-    animation = requestAnimationFrame(softRemover);
-
-    if (opacity < 0.01) {
-      cancelAnimationFrame(animation);
-    }
-  }
-  console.log('도/돼나?');
-
-  console.log(opacity);
   useEffect(() => {
     softRemover();
   }, [softRemover]);
 
-  window.addEventListener('click', () => console.log('click'));
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  function softRemover() {
+    if (opacity > 96) {
+      setTimeout(() => {
+        setOpacity(opacity - 1);
+      }, 100);
+    } else if (opacity > 5)
+      setTimeout(() => {
+        setOpacity(opacity - 8);
+      }, 50);
+  }
+
   return (
     <FeedbackContainerStyle //
       height={'60px'}
@@ -44,7 +59,7 @@ export function CompleteSavedMsg() {
       fontSize={'18px'}
       backgroundColor={'#85F9CF '}
       top={'13%'}
-      opacity={opacity}
+      opacity={`${opacity}%`}
     >
       저장이 완료되었습니다.
     </FeedbackContainerStyle>
@@ -52,12 +67,30 @@ export function CompleteSavedMsg() {
 }
 
 export function CompleteRemovedMsg() {
+  const [opacity, setOpacity] = useState(100);
+
+  useEffect(() => {
+    softRemover();
+  }, [softRemover]);
+
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  function softRemover() {
+    if (opacity > 96) {
+      setTimeout(() => {
+        setOpacity(opacity - 1);
+      }, 100);
+    } else if (opacity > 5)
+      setTimeout(() => {
+        setOpacity(opacity - 8);
+      }, 50);
+  }
   return (
     <FeedbackContainerStyle //
       height={'60px'}
       width={'275px'}
       fontSize={'18px'}
       top={'55%'}
+      opacity={`${opacity}%`}
     >
       삭제가 완료되었습니다.
     </FeedbackContainerStyle>
@@ -73,7 +106,7 @@ export const FeedbackContainerStyle = styled.div`
   font-size: ${(props) => props.fontSize || '16px'};
   top: ${(props) => props.top || '50%'};
   left: ${(props) => props.left || '50%'};
-  opacity: ${(props) => props.opacity || 0.5};
+  opacity: ${(props) => props.opacity || '100%'};
   transform: translateX(-50%) translateY(-50%);
   box-shadow: 0px 3px 5px rgba(0, 0, 0, 0.1);
   font-weight: 700;
